6de8c7ec1c73efe6406e23268d8d96e36cceb846
Tamal Saha <tamal@appscode.com>
2018-01-26 00:18:16 -0800

    Move ca cert commands to openssl (#24)

--- cmds/create.go
+++ cmds/create.go
@@ -1,17 +1,22 @@
 package cmds
 
 import (
+	onessl "github.com/appscode/onessl/cmds"
 	"github.com/spf13/cobra"
+	kubeadmapi "k8s.io/kubernetes/cmd/kubeadm/app/apis/kubeadm/v1alpha1"
 )
 
 func NewCmdCreate() *cobra.Command {
+	var (
+		certDir = kubeadmapi.DefaultCertificatesDir
+	)
 	cmd := &cobra.Command{
 		Use:               "create",
 		Short:             `create PKI`,
 		DisableAutoGenTag: true,
 	}
-	cmd.AddCommand(NewCmdCreateCA())
-	cmd.AddCommand(NewCmdCreateServer())
-	cmd.AddCommand(NewCmdCreateClient())
+	cmd.AddCommand(onessl.NewCmdCreateCA(certDir))
+	cmd.AddCommand(onessl.NewCmdCreateServer(certDir))
+	cmd.AddCommand(onessl.NewCmdCreateClient(certDir))
 	return cmd
 }
