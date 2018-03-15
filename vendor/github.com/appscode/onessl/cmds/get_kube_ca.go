821a426629a0711756783c362b91a6062a9625e0
tahsinrahman <tahsynx@gmail.com>
2018-03-15 18:55:41 +0000

    Add travis yaml

--- vendor/github.com/appscode/onessl/cmds/get_kube_ca.go
+++ vendor/github.com/appscode/onessl/cmds/get_kube_ca.go
@@ -6,9 +6,11 @@ import (
 	"os"
 
 	"github.com/appscode/kutil/tools/clientcmd"
+	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
 
+// kubectl config view --minify=true --flatten -o json | onessl jsonpath '{.clusters[0].cluster.certificate-authority-data}'
 func NewCmdGetKubeCA() *cobra.Command {
 	var (
 		kubeconfigPath string
@@ -21,14 +23,14 @@ func NewCmdGetKubeCA() *cobra.Command {
 		Run: func(cmd *cobra.Command, args []string) {
 			cfg, err := clientcmd.BuildConfigFromContext(kubeconfigPath, contextName)
 			if err != nil {
-				Fatal(fmt.Errorf("failed to read kubeconfig. Reason: %v", err))
+				Fatal(errors.Wrap(err, "failed to read kubeconfig"))
 			}
 			if len(cfg.CAData) > 0 {
 				fmt.Println(string(cfg.CAData))
 			} else if len(cfg.CAFile) > 0 {
 				data, err := ioutil.ReadFile(cfg.CAFile)
 				if err != nil {
-					Fatal(fmt.Errorf("failed to load ca file %s. Reason: %v", cfg.CAFile, err))
+					Fatal(errors.Wrapf(err, "failed to load ca file %s", cfg.CAFile))
 				}
 				fmt.Println(string(data))
 			}
--- vendor/github.com/appscode/onessl/cmds/root.go
+++ vendor/github.com/appscode/onessl/cmds/root.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"strings"
 
+	"github.com/appscode/go/analytics"
 	v "github.com/appscode/go/version"
 	"github.com/jpillora/go-ogle-analytics"
 	"github.com/spf13/cobra"
@@ -29,6 +30,7 @@ func NewRootCmd(version string) *cobra.Command {
 			})
 			if enableAnalytics && gaTrackingCode != "" {
 				if client, err := ga.NewClient(gaTrackingCode); err == nil {
+					client.ClientID(analytics.ClientID())
 					parts := strings.Split(c.CommandPath(), " ")
 					client.Send(ga.NewEvent(parts[0], strings.Join(parts[1:], "/")).Label(version))
 				}
@@ -41,9 +43,14 @@ func NewRootCmd(version string) *cobra.Command {
 	flag.CommandLine.Parse([]string{})
 	flag.Set("stderrthreshold", "ERROR")
 
-	rootCmd.AddCommand(NewCmdCreate())
-	rootCmd.AddCommand(NewCmdGet())
 	rootCmd.AddCommand(NewCmdBase64())
+	rootCmd.AddCommand(NewCmdCreate())
+	rootCmd.AddCommand(NewCmdEnvsubst())
+	rootCmd.AddCommand(NewCmdGet())
+	rootCmd.AddCommand(NewCmdJsonpath())
+	rootCmd.AddCommand(NewCmdSemver())
+	rootCmd.AddCommand(NewCmdHasKeys())
+	rootCmd.AddCommand(NewCmdWaitUntilReady())
 	rootCmd.AddCommand(v.NewCmdVersion())
 	return rootCmd
 }
