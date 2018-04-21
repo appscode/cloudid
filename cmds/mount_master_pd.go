e6251b778daca4a58ff59df57d8715cc93beb394
tamal <tamal@appscode.com>
2018-04-21 14:54:14 -0700

    Refactor

--- cmds/mount_master_pd.go
+++ cmds/mount_master_pd.go
@@ -1,12 +1,12 @@
 package cmds
 
 import (
-	"bufio"
 	"fmt"
 	"os"
 	"strings"
 	"time"
 
+	"github.com/golang/glog"
 	"github.com/pharmer/pre-k/lib"
 	"github.com/spf13/cobra"
 	"k8s.io/kubernetes/pkg/util/mount"
@@ -22,82 +22,72 @@ func NewCmdMountMasterPD() *cobra.Command {
 		Short:             "Mount persistant disk to master",
 		DisableAutoGenTag: true,
 		Run: func(cmd *cobra.Command, args []string) {
-			provider = lib.DetectCloudProvider()
-			var path string
-			var mount bool
-			mountDirectories()
-			switch provider {
-			case "aws":
-				path, mount = findAWSMasterPd()
-				break
-			case "gce":
-				path, mount = findGCEMasterPd()
-				break
-			default:
-				Fatal(fmt.Errorf("pd not supported"))
+			if provider != "aws" && provider != "gce" {
+				Fatal(fmt.Errorf("master pd not supported"))
 			}
-			if mount {
-				//https://github.com/kubernetes/kops/blob/0ef2fde69d076e9717839a587a57aec8562327e1/protokube/pkg/protokube/volume_mounter.go#L93
-				fmt.Println("Doing safe-format-and-mount path= ", path)
-				safeFormatAndMount(path)
-				mountDirectories()
+
+			safeFormatAndMount := &mount.SafeFormatAndMount{}
+			safeFormatAndMount.Interface = mount.New("")
+			safeFormatAndMount.Exec = mount.NewOsExec()
+
+			mounts, err := safeFormatAndMount.List()
+			if err != nil {
+				Fatal(err)
 			}
+			var existing []*mount.MountPoint
+			for _, m := range mounts {
+				if m.Path == mountPath {
+					existing = append(existing, &m)
+				}
+			}
+
+			if len(existing) == 0 {
+				fmt.Println("Creating mount directory ", mountPath)
+				if err = os.MkdirAll(mountPath, 0750); err != nil {
+					Fatal(err)
+				}
+				var device string
+				switch provider {
+				case "aws":
+					device = findAWSMasterPd()
+					break
+				case "gce":
+					device = findGCEMasterPd()
+					break
+				}
+				fmt.Println("Mounting device", device)
+				if err = safeFormatAndMount.FormatAndMount(device, mountPath, "", []string{}); err != nil {
+					Fatal(err)
+				}
+			} else {
+				glog.Infof("Device already mounted on %q, verifying it is our device", mountPath)
+
+				if len(existing) != 1 {
+					glog.Infof("Existing mounts unexpected")
+
+					for i := range mounts {
+						m := &mounts[i]
+						glog.Infof("%s\t%s", m.Device, m.Path)
+					}
+					Fatal(fmt.Errorf("found multiple existing mounts at %q", mountPath))
+				} else {
+					glog.Infof("Found existing mount at %q", mountPath)
+				}
+			}
+			createSymlinks()
 		},
 	}
-	//	cmd.Flags().StringVar(&diskId, "disk-id", "", "Persistant disk id")
+	cmd.Flags().StringVar(&provider, "provider", "", "Name of cloud provider")
 	return cmd
 }
 
-func safeFormatAndMount(devicePath string) {
-	safeFormatAndMount := &mount.SafeFormatAndMount{}
-	safeFormatAndMount.Interface = mount.New("")
-	safeFormatAndMount.Exec = mount.NewOsExec()
-
-	mounts, err := safeFormatAndMount.List()
-	if err != nil {
-		Fatal(err)
-	}
-	var existing []*mount.MountPoint
-	for _, m := range mounts {
-		if m.Path == mountPath {
-			existing = append(existing, &m)
-		}
-	}
-
-	if len(existing) == 0 {
-		fmt.Println("Creating mount directory ", mountPath)
-		if err = os.MkdirAll(mountPath, 0750); err != nil {
-			Fatal(err)
-		}
-		fmt.Println("Mounting device")
-		if err = safeFormatAndMount.FormatAndMount(devicePath, mountPath, "", []string{}); err != nil {
-			Fatal(err)
-		}
-	}
-}
-
-func findAWSMasterPd() (string, bool) {
+func findAWSMasterPd() string {
 	// ref: https://github.com/kubernetes/kubernetes/blob/fe18055adc9ce44a907999f99d239ead47345d5d/cluster/aws/templates/configure-vm-aws.sh#L53
-	// $ grep "/mnt/disks/master-pd" /proc/mounts
-	file, err := os.Open("/proc/mounts")
-	if err != nil {
-		Fatal(err)
-	}
-	defer file.Close()
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		if strings.Contains(scanner.Text(), mountPath) {
-			Fatal(fmt.Errorf("Master PD already mounted; won't remount"))
-		}
-	}
-	if err := scanner.Err(); err != nil {
-		Fatal(err)
-	}
-
+	// ref: https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/device_naming.html#available-ec2-device-names
 	// Waiting for master pd to be attached
 	attempt := 0
 	for true {
-		fmt.Println("Attempt %v to check for /dev/xvdb", attempt)
+		fmt.Printf("attempt %v to check for /dev/xvdb\n", attempt)
 		if _, err := os.Stat("/dev/xvdb"); err == nil {
 			fmt.Println("Found /dev/xvdb")
 			break
@@ -108,26 +98,24 @@ func findAWSMasterPd() (string, bool) {
 
 	// Mount the master PD as early as possible
 	//script.AddLine("/etc/fstab", "/dev/xvdb /mnt/master-pd ext4 noatime 0 0")
-	return "/dev/xvdb", true
+	return "/dev/xvdb"
 }
 
-func findGCEMasterPd() (string, bool) {
-	devicepath := "/dev/disk/by-id/google-master-pd"
-	if _, err := os.Stat(devicepath); os.IsNotExist(err) {
-		fmt.Println(devicepath + " does not exist")
-		// path does not exist
-		return "", false
+func findGCEMasterPd() string {
+	device := "/dev/disk/by-id/google-master-pd"
+	if _, err := os.Stat(device); os.IsNotExist(err) {
+		Fatal(fmt.Errorf("device %q not found", device))
 	}
 
-	outBytes, _ := lib.ExecCommand("ls", "-l", devicepath).Output()
+	outBytes, _ := lib.ExecCommand("ls", "-l", device).Output()
 	out := string(outBytes)
 	out = strings.TrimSpace(out)
 	relativePath := out[strings.LastIndex(out, " ")+1:]
-	return "/dev/disk/by-id/" + relativePath, true
+	return "/dev/disk/by-id/" + relativePath
 }
 
 //https://github.com/kubernetes/kubernetes/blob/a6b8c06380526d9631f1f965a61cd1aae2a5832d/cluster/gce/gci/configure-helper.sh#L387
-func mountDirectories() {
+func createSymlinks() {
 	//etcd
 	fmt.Println("createing etcd symlink")
 	os.MkdirAll(mountPath+"/var/lib/", 0700)
@@ -143,7 +131,6 @@ func mountDirectories() {
 		if err := lib.Run("useradd", "-s", "/sbin/nologin", "-d", "/var/etcd", "etcd"); err != nil {
 			Fatal(err)
 		}
-
 	}
 	if err := lib.Run("chown", "-R", "etcd", mountPath+"/var/lib/etcd"); err != nil {
 		Fatal(err)
@@ -151,5 +138,4 @@ func mountDirectories() {
 	if err := lib.Run("chgrp", "-R", "etcd", mountPath+"/var/lib/etcd"); err != nil {
 		Fatal(err)
 	}
-
 }
