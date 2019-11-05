2d3da29f938cffcbb6988c6b019be0dd40f43c61
Tamal Saha <tamal@appscode.com>
2019-11-04 16:04:30 -0800

    Various Makefile improvements
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- cmds/mount_master_pd.go
+++ cmds/mount_master_pd.go
@@ -1,3 +1,18 @@
+/*
+Copyright The Pharmer Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package cmds
 
 import (
@@ -6,10 +21,11 @@ import (
 	"strings"
 	"time"
 
+	"pharmer.dev/pre-k/lib"
+
 	"github.com/golang/glog"
 	"github.com/spf13/cobra"
 	"k8s.io/kubernetes/pkg/util/mount"
-	"pharmer.dev/pre-k/lib"
 )
 
 const mountPath = "/mnt/master-pd"
